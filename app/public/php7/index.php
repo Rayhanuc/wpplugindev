fe9362a1057abad49f707d6ee88c74f00c8c7e1a
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-07-28 01:22:47 +0600

    PHP file tested in localbyfilywheel

---
+++ app/public/php7/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP 7 Tutorial</title>
+
+	<!-- bootstrap css link -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- font-awesome css link -->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <!-- style css link -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- responsive css link -->
+    <link href="assets/css/responsive.css" rel="stylesheet">
+    <!-- css link end -->
+
+</head>
+<body>
+    <div class="heading">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1><?php echo "All PHP class of PHP 7 Tutorial" ?></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+	<div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="header-menu">
+                    <ul>
+                        <li><a href="class-1/class-1.php" target="_black"><?php echo "Class: 1" ?></a></li>
+                        <li><a href="php-1.2.php" target="_black"><?php echo "Class: php-1.2" ?></a></li>
+                        <li><a href="php-1.3.php" target="_black"><?php echo "Class: php-1.3" ?></a></li>
+                        <li><a href="php-1.4.php" target="_black"><?php echo "Class: php-1.4" ?></a></li>
+                        <li><a href="php-1.5.php" target="_black"><?php echo "Class: php-1.5" ?></a></li>
+                        <li><a href="php-1.6.php" target="_black"><?php echo "Class: php-1.6" ?></a></li>
+                        <li><a href="php-1.7.php" target="_black"><?php echo "Class: php-1.7" ?></a></li>
+                        <li><a href="php-1.8.php" target="_black"><?php echo "Class: php-1.8" ?></a></li>
+                        <li><a href="php-1.9.php" target="_black"><?php echo "Class: php-1.9" ?></a></li>
+                        <li><a href="php-1.10.php" target="_black"><?php echo "Class: php-1.10" ?></a></li>
+                        <li><a href="php-1.11.php" target="_black"><?php echo "Class: php-1.11" ?></a></li>
+                        <li><a href="php-1.12.php" target="_black"><?php echo "Class: php-1.12" ?></a></li>
+                        <li><a href="php-1.13.php" target="_black"><?php echo "Class: php-1.13" ?></a></li>
+                        <li><a href="php-1.14.php" target="_black"><?php echo "Class: php-1.14" ?></a></li>
+                        <li><a href="php-1.15.php" target="_black"><?php echo "Class: php-1.15" ?></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+	<!-- jquery js link -->
+    <script src="assets/js/jquery.min.js"></script>
+    <!-- bootstrap js link -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- main js link -->
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
