727fa31bb0bf7ec881356783aa07307f8c6b0e49
[Rayanuc] <[rayhanctg4@gmail.com]>
2019-10-04 15:15:50 +0600

    ২১.৩ - উইজেটে মিডিয়া অ্যাটাচমেন্ট এবং উইজেটের জাভাস্ক্রিপ্ট ইভেন্টস নিয়ে কাজ করা
    ২১.৩ - উইজেটে মিডিয়া অ্যাটাচমেন্ট এবং উইজেটের জাভাস্ক্রিপ্ট ইভেন্টস নিয়ে কাজ করা

--- app/public/wp-content/plugins/DemoWidget/demowidget.php
+++ app/public/wp-content/plugins/DemoWidget/demowidget.php
@@ -14,6 +14,7 @@
 */
 
 require_once plugin_dir_path(__FILE__)."widgets/class.demowidget.php";
+require_once plugin_dir_path(__FILE__)."widgets/class.Advertisementwidget2.php";
 
 
 function demowidget_load_textdomain(){
@@ -24,5 +25,15 @@ add_action('plugins_loaded','demowidget_load_textdomain');
 
 function demowidget_register(){
 	register_widget('DemoWidget');
+	register_widget('AdvertisementWidget');
 }
 add_action('widgets_init','demowidget_register');
+
+
+function demowidget_admin_enqueue_scripts($screen){
+	if ($screen == "widgets.php") {
+		wp_enqueue_media(  );
+		wp_enqueue_script("advertisment-widget-js", plugin_dir_url(__FILE__)."assets/js/media-gallery.js");
+	}
+}
+add_action("admin_enqueue_scripts","demowidget_admin_enqueue_scripts");
--- app/sql/local.wp_usermeta.sql
+++ app/sql/local.wp_usermeta.sql
@@ -17,8 +17,9 @@ INSERT INTO `wp_usermeta` VALUES
 (13,1,"wp_user_level","10"),
 (14,1,"dismissed_wp_pointers",""),
 (15,1,"show_welcome_panel","1"),
-(16,1,"session_tokens","a:1:{s:64:\"3276f4f23d6ee47e87613ea6a5bf18fadf9c0e90a6e9ba45738242e73eb44f02\";a:4:{s:10:\"expiration\";i:1570017225;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:128:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3907.0 Safari/537.36 Edg/79.0.279.0\";s:5:\"login\";i:1569844425;}}"),
+(16,1,"session_tokens","a:3:{s:64:\"3276f4f23d6ee47e87613ea6a5bf18fadf9c0e90a6e9ba45738242e73eb44f02\";a:4:{s:10:\"expiration\";i:1570017225;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:128:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3907.0 Safari/537.36 Edg/79.0.279.0\";s:5:\"login\";i:1569844425;}s:64:\"2f1512adf0755c06472533bfa5bc1794737d0804d87c16affa68a8e96d23dbeb\";a:4:{s:10:\"expiration\";i:1570082251;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:128:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3907.0 Safari/537.36 Edg/79.0.279.0\";s:5:\"login\";i:1569909451;}s:64:\"f2172c8bc9161bda59c7e57d8dd5c8afde503aa352f88aa2ef7421a0b2d189e7\";a:4:{s:10:\"expiration\";i:1570177155;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:128:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3907.0 Safari/537.36 Edg/79.0.279.0\";s:5:\"login\";i:1570004355;}}"),
 (17,1,"wp_dashboard_quick_press_last_post_id","165"),
 (18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}"),
 (19,1,"wp_user-settings","libraryContent=browse&editor=tinymce&widgets_access=off"),
-(20,1,"wp_user-settings-time","1569838639");
+(20,1,"wp_user-settings-time","1569838639"),
+(21,1,"meta-box-order_dashboard","a:4:{s:6:\"normal\";s:38:\"dashboard_right_now,dashboard_activity\";s:4:\"side\";s:39:\"dashboard_quick_press,dashboard_primary\";s:7:\"column3\";s:0:\"\";s:7:\"column4\";s:0:\"\";}");
