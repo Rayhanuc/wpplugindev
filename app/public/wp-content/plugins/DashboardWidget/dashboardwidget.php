23924ede2914048985196843468b81dc16ae4faa
[Rayanuc] <[rayhanctg4@gmail.com]>
2019-10-09 21:19:33 +0600

    ২১.৮ - ড্যাশবোর্ড উইজেটে ডেটা ফর্ম দেখানো এবং ডেটা সেভ করা
    ২১.৮ - ড্যাশবোর্ড উইজেটে ডেটা ফর্ম দেখানো এবং ডেটা সেভ করা

--- app/public/wp-content/plugins/DashboardWidget/dashboardwidget.php
+++ app/public/wp-content/plugins/DashboardWidget/dashboardwidget.php
@@ -20,28 +20,34 @@ function ddw_load_textdomain(){
 add_action('plugins_loaded','ddw_load_textdomain');
 
 function ddw_dashboard_widget(){
-	wp_add_dashboard_widget(
-		'demodashboardwidget', 
-		__('DashboardWidgetDemo','dashboardwidget'), 
-		'ddw_dashboardwidget_output'
-		/*control_callback, 
-		callback_args*/
-	);
+	if (current_user_can('edit_dashboard')) {
+		wp_add_dashboard_widget(
+			'demodashboardwidget', 
+			__('DashboardWidgetDemo','dashboardwidget'), 
+			'ddw_dashboardwidget_output',
+			'ddw_dashboardwidget_configure'
+		);
+	}else {
+		wp_add_dashboard_widget(
+			'demodashboardwidget', 
+			__('DashboardWidgetDemo','dashboardwidget'), 
+			'ddw_dashboardwidget_output'
+		);
+	}
 	wp_add_dashboard_widget(
 		'demodashboardwidget2', 
 		__('DashboardWidgetDemo2','dashboardwidget'), 
 		'ddw_dashboardwidget_output2'
-		/*control_callback, 
-		callback_args*/
 	);
 }
 add_action('wp_dashboard_setup','ddw_dashboard_widget');
 
 function ddw_dashboardwidget_output(){
+	$number_of_posts = get_option('dashboardwidget_nop', 5);
 	$feeds = array(
 		array(
 			'url' => 'https://wptavern.com/feed',
-			'items' => 5,
+			'items' => $number_of_posts,
 			'show_summary' => 0,
 			'show_author' => 0,
 			'show_date' => 0,
@@ -50,6 +56,26 @@ function ddw_dashboardwidget_output(){
 
 	wp_dashboard_primary_output( 'dashboardwidget', $feeds );
 
+}
+
+function ddw_dashboardwidget_configure(){
+	$number_of_posts = get_option('dashboardwidget_nop', 5);
+	if (isset($_POST['dashboard-widget-nonce']) && wp_verify_nonce($_POST['dashboard-widget-nonce'],'edit-dashboard-widget_demodashboardwidget')) {
+
+		if (isset($_POST['ddw_nop']) && $_POST['ddw_nop']>0) {
+			$number_of_posts = sanitize_text_field($_POST['ddw_nop']);
+			update_option('dashboardwidget_nop', $number_of_posts);
+		}
+	}
+	?>
+	<p>
+		
+	<label>Number of Posts: </label><br/>
+	<input type="text" name="ddw_nop" id="ddw_nop" class="widefat" value="<?php echo $number_of_posts; ?>">
+	</p>
+
+
+	<?php
 }
 
 
