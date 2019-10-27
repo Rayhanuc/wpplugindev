cf96ffd3cf6e41d0de80993f723b60586f6fb018
[Rayanuc] <[rayhanctg4@gmail.com]>
2019-10-27 14:26:51 +0600

    ২৪.২ - টেবিল আপডেট ও কলাম ড্রপ করা
    ২৪.২ - টেবিল আপডেট ও কলাম ড্রপ করা

--- app/public/wp-content/plugins/database-demo/dbdemo.php
+++ app/public/wp-content/plugins/database-demo/dbdemo.php
@@ -17,7 +17,7 @@
 
 
 
-define("DBDEMO_DB_VERSION","1.0");
+define("DBDEMO_DB_VERSION","1.3");
 
 function dbdemo_load_textdomain(){
 	load_plugin_textdomain('database-demo', false, plugin_dir_path(__FILE__)."/languages");
@@ -39,7 +39,32 @@ function dbdemo_init(){
 	dbDelta( $sql);
 
 	add_option("dbdemo_db_version", DBDEMO_DB_VERSION);
+
+	if (get_option("dbdemo_db_version") != DBDEMO_DB_VERSION) {
+		$sql = "CREATE TABLE {$table_name} (
+			id INT NOT NULL AUTO_INCREMENT,
+			name VARCHAR(250),
+			email VARCHAR(250),
+			age INT,
+			PRIMARY KEY (id)
+		);";
+		dbDelta( $sql);
+		update_option("dbdemo_db_version", DBDEMO_DB_VERSION);
+	}
 }
 
 register_activation_hook( __FILE__, "dbdemo_init" );
+
+
+function dbdemo_drop_column(){
+	global $wpdb;
+	$table_name = $wpdb->prefix.'persons';
+	if (get_option("dbdemo_db_version") != DBDEMO_DB_VERSION) {
+		$query = "ALTER TABLE {$table_name} DROP COLUMN age";
+		$wpdb->query($query);
+	}
+	update_option("dbdemo_db_version", DBDEMO_DB_VERSION);
+}
+add_action("plugins_loaded","dbdemo_drop_column");
+
 
