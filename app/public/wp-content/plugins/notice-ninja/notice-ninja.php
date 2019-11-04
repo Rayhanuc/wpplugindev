2a411900028ec6ea5cf7d58fac222dec8ee997f3
[Rayanuc] <[rayhanctg4@gmail.com]>
2019-11-04 20:54:45 +0600

    ২৫.৬ - ডাইনামিক অ্যাডমিন নোটিশ
    ২৫.৬ - ডাইনামিক অ্যাডমিন নোটিশ

--- app/public/wp-content/plugins/notice-ninja/notice-ninja.php
+++ app/public/wp-content/plugins/notice-ninja/notice-ninja.php
@@ -16,37 +16,48 @@
 */
 
 function noticeninja_admin_notice(){
-	?>
-	<div class="">
-		<h1>Here is a heading</h1>
-		<p>Hey, here is some information for you 1</p>
-	</div>
-	<div class="notice is-dismissible">
-		<h1>Here is a heading</h1>
-		<p>Hey, here is some information for you 1</p>
-	</div>
-	<div class="notice notice-error is-dismissible">
-		<p>Hey, here is some information for you 2</p>
-	</div>
-	<div class="notice notice-warning is-dismissible">
-		<p>Hey, here is some information for you 3</p>
-	</div>
-	<?php if(!(isset($_COOKIE['nn-close']) && $_COOKIE['nn-close'] == 1)) :?>
-		<div id="noticeninja" class="notice notice-success is-dismissible">
-			<p>Hey, here is some information for you 1</p>
-		</div>
-	<?php
-	endif;
+	global $pagenow;
+	/*
+	echo $pagenow;
+	this function will show you current page you are now
+	---------------------
+	Normal way
+	if ('index.php'== $pagenow || 'index.php'== $pagenow) {
+	another way:-
+	-----------
+	*/
+	if(in_array($pagenow,['themes.php','index.php','plugins.php'])){
+		// online link
+		$remote_data = wp_remote_get("https://rayhanuc.github.io/for-test/index2.html");
+
+		$remote_body = trim(wp_remote_retrieve_body($remote_data));
+		if($remote_body != ''){
+			?>
+			<div class="">
+				<h1>Here is a heading</h1>
+				<p>Hey, here is some information for you 1</p>
+			</div>
+			<div class="notice is-dismissible">
+				<h1>Here is a heading</h1>
+				<p>Hey, here is some information for you 1</p>
+			</div>
+			<div class="notice notice-error is-dismissible">
+				<p>Hey, here is some information for you 2</p>
+			</div>
+			<div class="notice notice-warning is-dismissible">
+				<p>Hey, here is some information for you 3</p>
+			</div>
+			<?php if(!(isset($_COOKIE['nn-close']) && $_COOKIE['nn-close'] == 1)) :?>
+				<div id="noticeninja" class="notice notice-success is-dismissible">
+					<?php echo $remote_body; ?>
+				</div>
+			<?php
+			endif;
+		}
+	}
 }
 add_action('admin_notices','noticeninja_admin_notice');
-
-
 
 add_action('admin_enqueue_scripts', function(){
 	wp_enqueue_script('noticeninja-js',plugin_dir_url( __FILE__ )."assets/js/script.js",array('jquery'),time(),true);
 });
-
-/*function noticeninja_assets(){
-	wp_enqueue_script('noticeninja-js',plugin_dir_url( __FILE__ )."/assets/js/script.js",array('jquery'),time(),true);
-}
-add_action( "admin_enqueue_scripts", "noticeninja_assets" );*/
